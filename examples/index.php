ae245d9534a4709747e36f6cfd214c38a585e353
bocharsky-bw <bocharsky.bw@gmail.com>
2016-12-04 09:38:54 +0200

    Tweak example

--- examples/index.php
+++ examples/index.php
@@ -2,23 +2,24 @@
 
 session_start();
 
-require __DIR__ . '/path/to/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 use \BW\Vkontakte as Vk;
 
 $vk = new Vk([
-    'client_id' => '4609415',
-    'client_secret' => 'JgeSQNKLZCT44qEsAyWG',
-    'redirect_uri' => 'http://localhost/test/',
+    'client_id' => '5759854',
+    'client_secret' => 'a556FovqtUBHArlXlAAO',
+    'redirect_uri' => 'http://localhost:8000',
 ]);
 
 if (isset($_GET['code'])) {
-    $vk->authenticate();
+    $vk->authenticate($_GET['code']);
     $_SESSION['access_token'] = $vk->getAccessToken();
     header('Location: ' . $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
     exit;
 } else {
-    $vk->setAccessToken($_SESSION['access_token']);
+    $accessToken = isset($_SESSION['access_token']) ? $_SESSION['access_token'] : null;
+    $vk->setAccessToken($accessToken);
 }
 
 $userId = $vk->getUserId();
@@ -32,9 +33,15 @@ $users = $vk->api('users.get', [
         'sex',
     ],
 ]);
+var_dump($users);
 
-print '<pre>';
-print_r($users);
-print '</pre>';
+?>
 
-print '<br><a href="' . $vk->getLoginUrl() . '">Sign In</a>';
+<br>
+<a href="<?= $vk->getLoginUrl() ?>">
+    <?php if ($userId) : ?>
+        Re-authenticate
+    <?php else : ?>
+        Authenticate
+    <?php endif ?>
+</a>
